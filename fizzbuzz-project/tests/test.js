11245c989f34a9a4619219acf7d9dcef03bbcde7
Timson Babajide <timson.babajide@gmail.com>
2017-04-08 09:57:17 +0100

    impliment jasmine test case

--- fizzbuzz-project/tests/test.js
+++ fizzbuzz-project/tests/test.js
@@ -1,42 +1,42 @@
 'use strict'
 
-var chai = require('chai');
-var expect = chai.expect;
+var jasmine = require('jasmine');
+var expect = jasmine.expect;
 
 var myApp = require('../app/mainApp.js');
 
 describe("Fizz Buzz tests ", function() {
 
   it("should return `Fizz` for number divisible by 3", function() {
-    expect(myApp.fizzBuzz(3)).to.equal('Fizz');
+    expect(fizzBuzz(3)).toBe('Fizz');
   });
 
   it("should return `Buzz` for number divisible by 5", function() {
-    expect(myApp.fizzBuzz(5)).to.equal('Buzz');
+    expect(fizzBuzz(5)).toBe('Buzz');
   });
 
   it("should return `FizzBuzz` for 15", function() {
-    expect(myApp.fizzBuzz(15)).to.equal('FizzBuzz');
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
   });
 
   it("should return `FizzBuzz` for 45", function() {
-    expect(myApp.fizzBuzz(45)).to.equal('FizzBuzz');
+    expect(fizzBuzz(45)).toBe('FizzBuzz');
   });
 
   it("should return `FizzBuzz` for 90", function() {
-    expect(myApp.fizzBuzz(90)).to.equal('FizzBuzz');
+    expect(fizzBuzz(90)).toBe('FizzBuzz');
   });
 
   it("should return `Fizz` for 63", function() {
-    expect(myApp.fizzBuzz(63)).to.equal('Fizz');
+    expect(fizzBuzz(63)).toBe('Fizz');
   });
 
   it("should return 7 since its indivisible by 3 and 5", function() {
-    expect(myApp.fizzBuzz(7)).to.equal(7);
+    expect(fizzBuzz(7)).toBe(7);
   });
 
   it("should return 101 since its indivisible by 3 and 5", function() {
-    expect(myApp.fizzBuzz(101)).to.equal(101);
-  })
+    expect(fizzBuzz(101)).toBe(101);
+  });
 
-})
+});
