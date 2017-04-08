1c04dc54c916e33030798e9aeff91a12b477b457
Timson Babajide <timson.babajide@gmail.com>
2017-04-08 11:38:26 +0100

    Final review

--- arithmetic-geometric-project/tests/test.js
+++ arithmetic-geometric-project/tests/test.js
@@ -1,5 +1,4 @@
-var jasmine = require('jasmine');
-var expect = jasmine.expect;
+
 
 var myApp = require('../app/arithmeticGeometric.js');
 
@@ -11,7 +10,7 @@ var myApp = require('../app/arithmeticGeometric.js');
     describe("Case for an empty array", function() {
 
       it("should return 0 for an empty array", function() {
-        expect(aritGeo([])).toEqual(0);
+        expect(myApp.aritGeo([])).toEqual(0);
       });
 
     });
@@ -19,15 +18,15 @@ var myApp = require('../app/arithmeticGeometric.js');
     describe("Case for an arithmetic sequence", function() {
 
       it("should return `Arithmetic` for [2, 4, 6, 8, 10]", function() {
-        expect(aritGeo([2, 4, 6, 8, 10])).toEqual('Arithmetic');
+        expect(myApp.aritGeo([2, 4, 6, 8, 10])).toEqual('Arithmetic');
       });
 
       it("should return `Arithmetic` for [5, 11, 17, 23, 29, 35, 41]", function() {
-        expect(aritGeo([5, 11, 17, 23, 29, 35, 41])).toEqual('Arithmetic');
+        expect(myApp.aritGeo([5, 11, 17, 23, 29, 35, 41])).toEqual('Arithmetic');
       });
 
       it("should return `Arithmetic` for [15, 10, 5, 0, -5, -10]", function() {
-        expect(aritGeo([15, 10, 5, 0, -5, -10])).toEqual('Arithmetic');
+        expect(myApp.aritGeo([15, 10, 5, 0, -5, -10])).toEqual('Arithmetic');
       });
 
     });
@@ -35,15 +34,15 @@ var myApp = require('../app/arithmeticGeometric.js');
     describe("Case for a geometric sequence", function() {
 
       it("should return `Geometric` for [2, 6, 18, 54, 162]", function() {
-        expect(aritGeo([2, 6, 18, 54, 162])).toEqual('Geometric');
+        expect(myApp.aritGeo([2, 6, 18, 54, 162])).toEqual('Geometric');
       });
 
       it("should return `Geometric` for [0.5, 3.5, 24.5, 171.5]", function() {
-        expect(aritGeo([0.5, 3.5, 24.5, 171.5])).toEqual('Geometric');
+        expect(myApp.aritGeo([0.5, 3.5, 24.5, 171.5])).toEqual('Geometric');
       });
 
       it("should return `Geometric` for [−128, 64, −32, 16, −8]", function() {
-        expect(aritGeo([-128, 64, -32, 16, -8])).toEqual('Geometric');
+        expect(myApp.aritGeo([-128, 64, -32, 16, -8])).toEqual('Geometric');
       });
 
     });
@@ -51,17 +50,17 @@ var myApp = require('../app/arithmeticGeometric.js');
     describe("Case for neither arithmetic nor geometric sequence", function() {
 
       it("should return -1 for [1, 2, 3, 5, 8]", function() {
-        expect(aritGeo([1, 2, 3, 5, 8])).toEqual(-1);
+        expect(myApp.aritGeo([1, 2, 3, 5, 8])).toEqual(-1);
       });
 
       it("should return -1 for [1, 3, 6, 10, 15]", function() {
-        expect(aritGeo([1, 3, 6, 10, 15])).toEqual(-1);
+        expect(myApp.aritGeo([1, 3, 6, 10, 15])).toEqual(-1);
       });
 
       it("should return -1 for [1, 8, 27, 64, 125]", function() {
-        expect(aritGeo([1, 8, 27, 64, 125])).toEqual(-1);
+        expect(myApp.aritGeo([1, 8, 27, 64, 125])).toEqual(-1);
       });
       
     });
   });
-})();``
+})();
--- getprimes-project/tests/test.js
+++ getprimes-project/tests/test.js
@@ -1,5 +1,3 @@
-var jasmine = require('jasmine');
-var expect = jasmine.expect;
 
 var myApp = require('../app/primes.js');
 
@@ -8,38 +6,52 @@ describe("get the prime numbers between nth number", function(){
 	describe("Test Case for constant value", function(){
 
 		it("should return empty array if 0 is pass to the function", function() {
-       	 	expect(myApp.getPrimes(0)).to.deep.equal([ ]);
+       	 	expect(myApp.getPrimes(0)).toEqual([ ]);
       	});
 
 		it("should return empty array if 2 is pass to the function", function() {
-        	expect(myApp.getPrimes(2)).to.deepEqual([ ]);
-        	
+        	expect(myApp.getPrimes(2)).toEqual([ ]);
+      	});
+
+
+      	it("should return invalid input if negative input is pass to the function", function() {
+        	expect(myApp.getPrimes(0.2)).toEqual([]);
       	});
 	});
 
 	describe("Test case for invalid input", function(){
 
 		it("should return invalid input if negative input is pass to the function", function() {
-        	expect(myApp.getPrimes(-2)).to.equal('Invalid input');
+        	expect(myApp.getPrimes(-2)).toEqual('Invalid input');
       	});
+
+      	it("should return invalid input if negative input is pass to the function", function() {
+        	expect(myApp.getPrimes(-234)).toEqual('Invalid input');
+      	});
+
+
 	});
 
 	describe("Test case for actual value", function(){
 
 		it("should return [ 3, 5, 7, 11, 13, 17, 19 ] for 20", function() {
-        	expect(myApp.getPrimes(10)).to.deep.equal('[ 3, 5, 7 ]');
+        	expect(myApp.getPrimes(10)).toEqual([ 3, 5, 7 ]);
+      	});
+
+      	it("should return [ 3, 5, 7, 11, 13, 17, 19 ] for 20", function() {
+        	expect(myApp.getPrimes(20.5)).toEqual([ 3, 5, 7, 11, 13, 17, 19 ]);
       	});
 
 		it("should return [3, 5, 7, 11, 13, 17, 19, 23, 29] for 30", function() {
-        	expect(myApp.getPrimes(30)).to.deep.equal([ 3, 5, 7, 11, 13, 17, 19, 23, 29 ]);
+        	expect(myApp.getPrimes(30)).toEqual([ 3, 5, 7, 11, 13, 17, 19, 23, 29 ]);
       	});
 
 		it("should return [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53] for 53", function() {
-        	expect(myApp.getPrimes(53)).to.deep.equal([ 3, 5, 7,  11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53 ]);
+        	expect(myApp.getPrimes(53)).toEqual([ 3, 5, 7,  11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53 ]);
       	});
 
 		it("should return [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79] for 79", function() {
-        	expect(myApp.getPrimes(79)).to.deep.equal([ 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79 ]);
+        	expect(myApp.getPrimes(79)).toEqual([ 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79 ]);
       	});
 
 	});
