cbd5378b04768d7e1db85a3ed944ceb9ef1eb9bd
Timson Babajide <timson.babajide@gmail.com>
2017-04-08 09:30:46 +0100

    jasmine test case inplimented

--- arithmetic-geometric-project/tests/test.js
+++ arithmetic-geometric-project/tests/test.js
@@ -1,14 +1,17 @@
-var chai = require('chai');
-var expect = chai.expect;
+var jasmine = require('jasmine');
+var expect = jasmine.expect;
 
 var myApp = require('../app/arithmeticGeometric.js');
+
+ (function(){
+  'use strict';
 
   describe("Determine the sequence of an array of numbers: ", function() {
 
     describe("Case for an empty array", function() {
 
       it("should return 0 for an empty array", function() {
-        expect(myApp.aritGeo([])).to.equal(0);
+        expect(aritGeo([])).toEqual(0);
       });
 
     });
@@ -16,15 +19,15 @@ var myApp = require('../app/arithmeticGeometric.js');
     describe("Case for an arithmetic sequence", function() {
 
       it("should return `Arithmetic` for [2, 4, 6, 8, 10]", function() {
-        expect(myApp.aritGeo([2, 4, 6, 8, 10])).to.equal('Arithmetic');
+        expect(aritGeo([2, 4, 6, 8, 10])).toEqual('Arithmetic');
       });
 
       it("should return `Arithmetic` for [5, 11, 17, 23, 29, 35, 41]", function() {
-        expect(myApp.aritGeo([5, 11, 17, 23, 29, 35, 41])).to.equal('Arithmetic');
+        expect(aritGeo([5, 11, 17, 23, 29, 35, 41])).toEqual('Arithmetic');
       });
 
       it("should return `Arithmetic` for [15, 10, 5, 0, -5, -10]", function() {
-        expect(myApp.aritGeo([15, 10, 5, 0, -5, -10])).to.equal('Arithmetic');
+        expect(aritGeo([15, 10, 5, 0, -5, -10])).toEqual('Arithmetic');
       });
 
     });
@@ -32,15 +35,15 @@ var myApp = require('../app/arithmeticGeometric.js');
     describe("Case for a geometric sequence", function() {
 
       it("should return `Geometric` for [2, 6, 18, 54, 162]", function() {
-        expect(myApp.aritGeo([2, 6, 18, 54, 162])).to.equal('Geometric');
+        expect(aritGeo([2, 6, 18, 54, 162])).toEqual('Geometric');
       });
 
       it("should return `Geometric` for [0.5, 3.5, 24.5, 171.5]", function() {
-        expect(myApp.aritGeo([0.5, 3.5, 24.5, 171.5])).to.equal('Geometric');
+        expect(aritGeo([0.5, 3.5, 24.5, 171.5])).toEqual('Geometric');
       });
 
       it("should return `Geometric` for [−128, 64, −32, 16, −8]", function() {
-        expect(myApp.aritGeo([-128, 64, -32, 16, -8])).to.equal('Geometric');
+        expect(aritGeo([-128, 64, -32, 16, -8])).toEqual('Geometric');
       });
 
     });
@@ -48,16 +51,17 @@ var myApp = require('../app/arithmeticGeometric.js');
     describe("Case for neither arithmetic nor geometric sequence", function() {
 
       it("should return -1 for [1, 2, 3, 5, 8]", function() {
-        expect(myApp.aritGeo([1, 2, 3, 5, 8])).to.equal(-1);
+        expect(aritGeo([1, 2, 3, 5, 8])).toEqual(-1);
       });
 
       it("should return -1 for [1, 3, 6, 10, 15]", function() {
-        expect(myApp.aritGeo([1, 3, 6, 10, 15])).to.equal(-1);
+        expect(aritGeo([1, 3, 6, 10, 15])).toEqual(-1);
       });
 
       it("should return -1 for [1, 8, 27, 64, 125]", function() {
-        expect(myApp.aritGeo([1, 8, 27, 64, 125])).to.equal(-1);
+        expect(aritGeo([1, 8, 27, 64, 125])).toEqual(-1);
       });
       
     });
-})
+  });
+})();``
