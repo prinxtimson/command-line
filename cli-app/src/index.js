78e74fd524bcb8bb18db17ef7cce087c8f26a0b4
Timson Babajide <timson.babajide@gmail.com>
2017-04-12 04:37:32 +0100

    less complex CLI app

--- cli-app/src/index.js
+++ cli-app/src/index.js
@@ -8,27 +8,8 @@ const inquirer = require('inquirer');
 const Spinner = CLI.Spinner;
 const axios = require('axios');
 
-  var questions = [
-    {
-      name: 'username',
-      type: 'input',
-      message: 'Enter location of the github users you want to find: ',
-      validate: function( value ) {
-        if (value.length) {
-          return true;
-        } else {
-          return 'Please enter the location of the users u want to find:';
-        }
-      }
-    }
-  ]
 
-   let locat;
-   inquirer.prompt(questions).then(function(res){
-       locat = res.username;
-    })
-
-axios.get('https://api.github.com/search/users?q=location:'+locat).then(function(response){
+axios.get('https://api.github.com/search/users?q=location:lagos').then(function(response){
 	var item = response.data.items;
   for(var u=0; u<item.length; u++){
     console.log(chalk.yellow('Username: ')+chalk.green(item[u].login));
